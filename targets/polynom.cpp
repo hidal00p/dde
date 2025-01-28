d3552b7c42f3d21e9a369119510b054db06a9702
Anton Kovalov <anton.kov.hack@proton.me>
2025-01-28 11:41:28 +0100

    refactor: move around functions in demo

--- targets/polynom.cpp
+++ targets/polynom.cpp
@@ -9,43 +9,9 @@ constexpr double TOL = 1e-6;
 constexpr uint8_t MAX_ITER = 20;
 constexpr char const *DELIM = "========";
 const static std::string
-    path("/home/hidaloop/.folder/random/pinenv/dde/scripts/prog.gr");
+    GRAPH_PATH("/home/hidaloop/.folder/random/pinenv/dde/scripts/prog.gr");
 
-namespace lib {
-double g(double x) { return (x - 1.345); }
-
-double p(double x) {
-  double y = (x + 6.09);
-  double z = (x * x - 1764);
-  return y * z;
-}
-
-double f(double x) { return g(x) * p(x); }
-} // namespace lib
-
-void newton(double x0) {
-  uint8_t i = 0;
-  double eps;
-  do {
-    // Forward step
-    DDE_START
-    DDE_VAR("x", double x = x0)
-    DDE_OUTPUT("f_x", double f_x = 0);
-    f_x = lib::f(x);
-    DDE_STOP
-
-    // Newton update
-    DDE_DUMP_GRAPH
-    Graph gr(path);
-    gr.backprop();
-    double df_dx = gr.parsed["x"]->der;
-
-    x0 = x - f_x / df_dx;
-    eps = std::abs(f_x);
-
-  } while (++i < MAX_ITER && eps > TOL);
-  std::cout << "Solution: " << x0 << std::endl;
-}
+void newton(double);
 
 int main() {
   std::vector<double> guesses = {-100.0, -10, 10, 100.0};
@@ -60,4 +26,40 @@ int main() {
   }
 
   return 0;
+}
+
+namespace lib {
+double g(double x) { return (x - 1.345); }
+
+double p(double x) {
+  double y = (x + 6.09);
+  double z = (x * x - 1764); // +-42
+  return y * z;
+}
+
+double f(double x) { return g(x) * p(x); }
+} // namespace lib
+
+void newton(double x0) {
+  uint8_t i = 0;
+  double eps = 10;
+  do {
+    // Forward step
+    DDE_START
+    DDE_VAR("x", double x = x0)
+    DDE_OUTPUT("f_x", double f_x = 0);
+    f_x = lib::f(x);
+    DDE_STOP
+
+    // Newton update
+    DDE_DUMP_GRAPH
+    Graph gr(GRAPH_PATH);
+    gr.backprop();
+    double df_dx = gr.parsed["x"]->der;
+
+    x0 = x - f_x / df_dx;
+    eps = std::abs(f_x);
+
+  } while (++i < MAX_ITER && eps > TOL);
+  std::cout << "Solution: " << x0 << std::endl;
 }
