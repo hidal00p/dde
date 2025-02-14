7d6b61947ea47c2d5469571e85f81879370c7679
Anton Kovalov <anton.kov.hack@proton.me>
2025-02-14 16:17:30 +0100

    test: test binary op ctx construction

--- src/dde/transform_ctx.cpp
+++ src/dde/transform_ctx.cpp
@@ -4,7 +4,7 @@
 
 namespace binary_op {
 ctx *get_bop_operands(INS ins) {
-  static const uint8_t SRC_IDX = 1, DEST_IDX = 0;
+  constexpr uint8_t SRC_IDX = 1, DEST_IDX = 0;
   ctx *bop_ctx = new ctx();
   bop_ctx->ins = ins;
 
--- tests/unit/test_transform_ctx.cpp
+++ tests/unit/test_transform_ctx.cpp
@@ -3,7 +3,7 @@
 #include "pin.H"
 #include "transform_ctx.h"
 
-TEST(test_bop_ctx) {
+TEST(test_bop_ctx_mem_to_reg) {
   OPR dest = {.type = _REGSTR, .val = {.reg = REG::REG_EAX}};
   OPR src = {.type = _MEM};
   INS ins(dest, src);
@@ -12,4 +12,27 @@ TEST(test_bop_ctx) {
   CHECK(bop->dest.type == OprType::REGSTR &&
         bop->dest.origin.reg == REG::REG_EAX);
   CHECK(bop->src.type == OprType::MEM);
+}
+
+TEST(test_bop_ctx_reg_to_reg) {
+  OPR dest = {.type = _REGSTR, .val = {.reg = REG::REG_EDX}};
+  OPR src = {.type = _REGSTR, .val = {.reg = REG::REG_EAX}};
+  INS ins(dest, src);
+
+  binary_op::ctx *bop = binary_op::get_bop_operands(ins);
+  CHECK(bop->dest.type == OprType::REGSTR &&
+        bop->dest.origin.reg == REG::REG_EDX);
+  CHECK(bop->src.type == OprType::REGSTR &&
+        bop->src.origin.reg == REG::REG_EAX);
+}
+
+TEST(test_bop_ctx_imm_to_reg) {
+  OPR dest = {.type = _REGSTR, .val = {.reg = REG::REG_EDX}};
+  OPR src = {.type = _IMM, .val = {.imm = 42}};
+  INS ins(dest, src);
+
+  binary_op::ctx *bop = binary_op::get_bop_operands(ins);
+  CHECK(bop->dest.type == OprType::REGSTR &&
+        bop->dest.origin.reg == REG::REG_EDX);
+  CHECK(bop->src.type == OprType::IMM && bop->src.origin.imm == 42);
 }
