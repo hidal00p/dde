7d22fb0f713ab4d868479be860308d2730940573
Anton Kovalov <anton.kov.hack@proton.me>
2025-07-03 10:24:40 +0000

    refactor: bump c++ version, elab test file

--- targets/test.cpp
+++ targets/test.cpp
@@ -15,12 +15,29 @@ int main() {
 
   double x = 3.12;
   dde::start();
+
   dde::var(&x, "x");
 
   double y = f(x);
   dde::output(&y, "f");
 
   dde::stop();
+  dde::dump_graph();
+
+  std::vector<double> a = {1, 1};
+  std::vector<double> b = {1, 2};
+
+  dde::start();
+  for (int i = 0; i < a.size(); i++) {
+    dde::var(&a[i], "a", i);
+    dde::var(&b[i], "b", i);
+  }
+
+  double yd = 0;
+  dot(a, b, yd);
+  dde::output(&yd, "dot");
+  dde::stop();
+  dde::dump_graph();
 
   return 0;
 }
