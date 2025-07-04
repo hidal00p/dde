6cd4017b6b149233517d398fc215b50699abceef
Anton Kovalov <anton.kov.hack@proton.me>
2025-07-04 17:19:42 +0000

    fix: forces trig to resolve to sin/cos sigs
        * Unfortunately calls to intrinsics do not always resolve to their
          corresponding symbols, e.g. a call to sin would resolve to some f64xsubf128 symbol... Fixing this would required more thought.

--- targets/mylib.cpp
+++ targets/mylib.cpp
@@ -2,20 +2,22 @@
 #include <cmath>
 
 namespace mylib {
+double mysin(double x) { return std::sin(x); }
+double mycos(double x) { return std::cos(x); }
 double g(double x) { return (x - 1.345); }
 
 double p(double x) { return (x + 4); }
 
-double f1(double x) { return std::cos(x) * g(x) * p(x); }
+double f1(double x) { return mycos(x) * g(x) * p(x); }
 
 double f2(double x) {
 
   double y = 1.0;
-  for(int i = 0; i < 4; i++) {
+  for (int i = 0; i < 4; i++) {
     if (i % 2 == 0)
-      y = std::sin(x + y);
+      y = mysin(x + y);
     else
-      y = std::cos(x + y);
+      y = mycos(x + y);
   }
 
   return y;
