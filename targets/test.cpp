842dda061c21b4c23eda090659b55f367decd2ee
Anton Kovalov <anton.kov.hack@proton.me>
2025-07-04 17:31:36 +0000

    feat: add a use-case selection

--- targets/test.cpp
+++ targets/test.cpp
@@ -10,33 +10,34 @@ void dot(std::vector<double> &a, std::vector<double> &b, double &y) {
   }
 }
 
-double f(double x) { return x * x + 1.345; }
+int main(int argc, char **argv) {
+  assert(argc == 2);
+  int use_case = std::atoi(argv[1]);
 
-int main() {
+  if (use_case == 1) {
+    double x = 3.12;
+    dde::start();
+    dde::var(&x, "x");
+    double y = mylib::f2(x);
+    dde::output(&y, "f");
+    dde::stop();
+    dde::dump_graph();
+  } else {
+    std::vector<double> a = {1, 1};
+    std::vector<double> b = {1, 2};
 
-  double x = 3.12;
-  dde::start();
-  dde::var(&x, "x");
-  double y = mylib::f2(x);
-  dde::output(&y, "f");
+    dde::start();
+    for (int i = 0; i < a.size(); i++) {
+      dde::var(&a[i], "a", i);
+      dde::var(&b[i], "b", i);
+    }
 
-  dde::stop();
-  dde::dump_graph();
-
-  // std::vector<double> a = {1, 1};
-  // std::vector<double> b = {1, 2};
-
-  // dde::start();
-  // for (int i = 0; i < a.size(); i++) {
-  //   dde::var(&a[i], "a", i);
-  //   dde::var(&b[i], "b", i);
-  // }
-
-  // double yd = 0;
-  // dot(a, b, yd);
-  // dde::output(&yd, "dot");
-  // dde::stop();
-  // dde::dump_graph();
+    double yd = 0;
+    dot(a, b, yd);
+    dde::output(&yd, "dot");
+    dde::stop();
+    dde::dump_graph();
+  }
 
   return 0;
 }
